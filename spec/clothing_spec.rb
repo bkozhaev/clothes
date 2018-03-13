cd67f63881f6ac5fef30ac2be7e6eda931b009f7
baur <bkozhaev@gmail.com>
2018-03-13 15:29:48 +0400

    adding spec testing files for wardrob and clothing classes

--- spec/clothing_spec.rb
+++ spec/clothing_spec.rb
@@ -1,9 +1,28 @@
 require 'rspec'
+require_relative '../lib/clothing'
 
-describe 'My behaviour' do
+describe 'clothing.rb' do
+  let(:clothing) do
+    Clothing.from_file(__dir__ + '/../data/01.txt')
+  end
 
-  it 'should do something' do
+  it 'should return instance of clothing' do
+    expect(clothing).to be_an_instance_of(Clothing)
+  end
 
-    true.should == false
+  it 'should return a type of clothing' do
+    expect(clothing.type).to eql('Головной убор')
+  end
+
+  it 'should return a name of clothing' do
+    expect(clothing.name).to eql('Шапка-ушанка')
+  end
+
+  it 'should return a max_temp of clothing' do
+    expect(clothing.max_temp).to eql(-20)
+  end
+
+  it 'should return arguments of a class instance as a string' do
+    expect(clothing.to_s).to eql('Шапка-ушанка (Головной убор) -20..-5')
   end
 end
---
+++ spec/wardrob_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec'
+
+require_relative '../lib/wardrob'
+require_relative '../lib/clothing'
+
+describe 'wardrob.rb' do
+  let(:wardrob) do
+    files_dir = ["#{__dir__}/../data/01.txt", "#{__dir__}/../data/02.txt"]
+    Wardrob.from_dir(files_dir)
+  end
+
+  it 'should return an instance of wardrob class' do
+    expect(wardrob).to be_an_instance_of(Wardrob)
+  end
+
+  it 'should be an instance of array' do
+    expect(wardrob.collection).to be_an_instance_of(Array)
+  end
+
+  it 'should should be an instance of array' do
+    expect(wardrob.get_suitable_clothing(20)).to be_an_instance_of(Array)
+  end
+
+  it 'should return a string from clothing instance' do
+    expect(wardrob.get_suitable_clothing(20)).to eql('Шлепанцы (Обувь) 20..40')
+  end
+end
