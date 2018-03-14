17a38141769fb4f14c5219eb1e612451aced6c87
baur <bkozhaev@gmail.com>
2018-03-14 12:23:14 +0400

    fixed spec files following comments of sansey

--- spec/clothing_spec.rb
+++ spec/clothing_spec.rb
@@ -1,28 +1,25 @@
 require 'rspec'
 require_relative '../lib/clothing'
 
-describe 'clothing.rb' do
+describe '.from_file(file_path)' do
   let(:clothing) do
     Clothing.from_file(__dir__ + '/../data/01.txt')
   end
 
-  it 'should return instance of clothing' do
+  it 'should return instance of class Clothing' do
     expect(clothing).to be_an_instance_of(Clothing)
   end
 
-  it 'should return a type of clothing' do
+  it 'should fill attributes: name, type, max_temp, min_temp' do
     expect(clothing.type).to eql('Головной убор')
-  end
-
-  it 'should return a name of clothing' do
     expect(clothing.name).to eql('Шапка-ушанка')
-  end
-
-  it 'should return a max_temp of clothing' do
     expect(clothing.max_temp).to eql(-20)
+    expect(clothing.min_temp).to eql(-5)
   end
 
-  it 'should return arguments of a class instance as a string' do
-    expect(clothing.to_s).to eql('Шапка-ушанка (Головной убор) -20..-5')
+  describe '.to_s' do
+    it 'should return a string file containing attributes: name, tyep, max_temp, min_temp' do
+      expect(clothing.to_s).to eql('Шапка-ушанка (Головной убор) -20..-5')
+    end
   end
 end
--- spec/wardrob_spec.rb
+++ spec/wardrob_spec.rb
@@ -3,25 +3,41 @@ require 'rspec'
 require_relative '../lib/wardrob'
 require_relative '../lib/clothing'
 
-describe 'wardrob.rb' do
+describe '.from_dir(clothing, file_path)' do
   let(:wardrob) do
-    files_dir = ["#{__dir__}/../data/01.txt", "#{__dir__}/../data/02.txt"]
+    files_dir = ["#{__dir__}/../data/01.txt", "#{__dir__}/../data/04.txt"]
     Wardrob.from_dir(Clothing, files_dir)
   end
 
-  it 'should return an instance of wardrob class' do
-    expect(wardrob).to be_an_instance_of(Wardrob)
-  end
-
-  it 'should be an instance of array' do
+  it 'should return array of Clothing collections' do
     expect(wardrob.collection).to be_an_instance_of(Array)
   end
 
-  it 'should should be an instance of array' do
-    expect(wardrob.get_suitable_clothing(20)).to be_an_instance_of(Array)
+  it 'returns the instance of Clothing from collection Array' do
+    expect(wardrob.collection.first).to be_an_instance_of(Clothing)
   end
 
-  it 'should return a string from clothing instance' do
-    expect(wardrob.get_suitable_clothing(20)[0].to_s).to eql('Шлепанцы (Обувь) 20..40')
+  it 'should return the attributes of Clothing: name, type, max_temp, min_temp' do
+    expect(wardrob.collection.first.name).to eq("Шапка-ушанка")
+    expect(wardrob.collection.first.type).to eq("Головной убор")
+    expect(wardrob.collection.first.max_temp).to eq(-20)
+    expect(wardrob.collection.first.min_temp).to eq(-5)
+  end
+
+  describe '.get_suitable_clothing(temperature)' do
+    let(:clothes) do
+      wardrob.get_suitable_clothing(-5)
+    end
+
+    it 'should return an array of instances' do
+      expect(clothes).to be_an_instance_of(Array)
+    end
+
+    it 'should return clothing which suites to -5 parameter' do
+      expect(clothes.first.name).to eq('Шапка-ушанка')
+      expect(clothes.last.name).to eq('Джинсы')
+      expect(clothes.first.min_temp).to eq(-5)
+      expect(clothes.first.min_temp).to eq(-5)
+    end
   end
 end
